8a4ae899408c2a93184c152c647570dac2215dd2
hmansur7 <haarishmansur7@gmail.com>
2025-03-31 23:48:37 -0400

    Small changes for dev

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Welcome to the LMS API!")),
     path('admin/', admin.site.urls),
     path('api/', include('content.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
